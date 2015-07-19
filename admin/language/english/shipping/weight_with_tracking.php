1df297dc3441eabdc20de95112655b20b1429fef
Ivan Derevianko <drussilla7@gmail.com>
2015-07-19 22:51:53 +0200

    Init commit

---
+++ admin/language/english/shipping/weight_with_tracking.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'PostNL Shipping With Tracking Code';
+
+// Text
+$_['text_shipping']    = 'Shipping';
+$_['text_success']     = 'Success: You have modified PostNL shipping with tracking code!';
+$_['text_edit']        = 'Edit PostNL Shipping With Tracking Code';
+
+// Entry
+$_['entry_rate']       = 'Rates';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc..';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify weight based shipping!';
---
+++ catalog/language/english/shipping/weight_with_tracking.php
@@ -0,0 +1,3 @@
+<?php
+// Text
+$_['text_title']  = 'PostNL Shipping With Tracking Code';
